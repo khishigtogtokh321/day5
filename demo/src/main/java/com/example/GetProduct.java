da237df7b6f32e2e50fb192fb4a997fb82f23936
Tergel <tergelll0525@gmail.com>
2025-06-09 13:14:34 +0800

    aldaa zohitsuulah nemsen

--- demo/src/main/java/com/example/GetProduct.java
+++ demo/src/main/java/com/example/GetProduct.java
@@ -21,11 +21,26 @@ public class GetProduct {
         if (code.trim().isEmpty()) {
             throw new IllegalArgumentException("Барааны код хоосон байж болохгүй.");
         }
+        if (code.length() == 3) {
+            char firstChar = code.charAt(0);
+            char secondChar = code.charAt(1);
+            char thirdChar = code.charAt(2);
+            if(!Character.isLetter(firstChar) && Character.isDigit(secondChar) && Character.isDigit(thirdChar)){
+                throw new IllegalArgumentException("Барааны код буруу байна.");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Барааны код буруу байна.");
+        }
 
         System.out.print("Тоо ширхэг: ");
         double quantity = read.nextDouble();
         if (quantity < 0) {
             throw new IllegalArgumentException("Тоо ширхэг сөрөг байж болохгүй.");
+        }
+        double MAX = 50.0;
+        if (quantity > MAX) {
+            throw new IllegalArgumentException("Тоо ширхэг хэтэрсэн.");
         }
 
         System.out.print("Барааны үнэ: ");
